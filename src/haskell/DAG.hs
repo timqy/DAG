2b276fb8bc9790f4b2ccdc854d5215b7869fb4c0
Tim Hedberg <dv13thg@cs.umu.se>
2016-03-12 12:46:04 +0100

    added stuff =D

--- src/haskell/DAG.hs
+++ src/haskell/DAG.hs
@@ -1,15 +1,30 @@
+{-# LANGUAGE FlexibleInstances #-}
 module DAG (Graph, addVertex) where
 import Data.Char
 import Data.List
 import Data.Maybe
 
-type Node n = (Int, n)
-type Edge e = (Int, Int, e)
+type Node a = (Int, a)
+type Edge a = (Int, Int, a)
 
-data Graph g = Graph {
-    nodes :: [Node g],
-    edges :: [Edge g]
+data Graph a = Graph {
+    nodes :: [Node a],
+    edges :: [Edge a]
 } deriving (Show, Eq, Read)
+
+class Weight a where
+	wcmp :: a -> a -> Bool
+	wsum :: a -> a -> a
+
+-- Implementation of Weight as an integer
+instance Weight (Integer) where
+	wcmp x y = x > y
+	wsum x y = x + y
+
+-- Another implementation of Weight
+instance Weight ([Char]) where
+	wcmp x y = x > y
+	wsum x y = x ++ y 
 
 emptyGraph :: Graph a
 emptyGraph = Graph [] []
@@ -24,9 +39,10 @@ addVertex (Graph nodes edges) w = ((Graph ((length nodes, w):nodes) edges), (len
 --addEdge
 addEdge :: Graph a -> Int -> Int -> a -> Graph a
 addEdge (Graph nodes edges) n1 n2 w
-    |((existNode (Graph nodes edges) n1) /= Nothing
-        && (existNode (Graph nodes edges) n2) /= Nothing
-        && (not (existEdgeBetween (Graph nodes edges) n1 n2))) = Graph nodes ((n1, n2, w):edges)
+    |((existNode (Graph nodes edges) n1) /= Nothing)
+        && ((existNode (Graph nodes edges) n2) /= Nothing)
+        && (not (existEdgeBetween (Graph nodes edges) n2 n1))
+        && (not (existEdgeBetween (Graph nodes edges) n1 n2)) = (Graph nodes ((n1, n2, w):edges))
     |otherwise = error "Nodes does not exist or already exist edge between them"
 
 existNode :: Graph a -> Int -> Maybe (Int)
@@ -36,7 +52,22 @@ hasIncomingEdge :: Graph a -> Int -> Bool
 hasIncomingEdge (Graph nodes edges) n = (length (filter (\(_,b,_) -> b == n) edges)) > 0
 
 existEdgeBetween :: Graph a -> Int -> Int -> Bool
-existEdgeBetween (Graph nodes edges) n1 n2 = (filter (\(b,c,_) -> b == n1 && c == n2) edges) /= []
+existEdgeBetween (Graph nodes edges) n1 n2 = (length (filter (\(b,c,_) -> b == n1 && c == n2) edges)) > 0
+
+getStartNodes :: Graph a -> [Int]
+getStartNodes a = (nub(getAllOutgoing a)) \\ (nub(getAllIncoming a))
+
+getAllOutgoing :: Graph a -> [Int]
+getAllOutgoing (Graph nodes edges) = map (getOutgoing) edges
+
+getAllIncoming :: Graph a -> [Int]
+getAllIncoming (Graph nodes edges) = map (getIncoming) edges
+
+getOutgoing :: Edge a -> Int
+getOutgoing (a,_,_) = a
+
+getIncoming :: Edge a -> Int
+getIncoming (_,a,_) = a
 
 -- vlist = topological_ordering
 -- Computes a topological ordering of the DAG and returns the corresponding list of vertex identifiers. Recall that a topological ordering is a linear ordering of a directed graph such that if the vertices of the graph are layed out from left to right in that order, then all edges go from left to right.
